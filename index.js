2fc0f75625a8c8a5f3023349ff67daeb6fa29513
sanavalos <santiago.avalos@outlook.com>
2022-11-21 18:48:00 -0300

    file line counter ready for gihthub

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+try {
+  const data = fs.readFileSync("./service.js", "utf8");
+  cleanCode(data);
+} catch (err) {
+  console.error(err);
+}
+
+function cleanCode(data) {
+  data = data.split("\n");
+  for (let i = 0; i < data.length; i++) {
+    data[i] = data[i].replace(/(\r\n|\n|\r|\s)/gm, "");
+  }
+  main(data);
+}
+
+function countLines(data) {
+  let validLines = 0;
+  let insideComment = false;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].startsWith("/*")) {
+      insideComment = true;
+    } else if (data[i].includes("*/")) {
+      insideComment = false;
+    } else if (
+      !insideComment &&
+      !(data[i].startsWith("//") || data[i] == "" || data[i].startsWith("*"))
+    ) {
+      validLines++;
+    }
+  }
+  return validLines;
+}
+
+function main(data) {
+  return console.log("The number of valid lines of code is:", countLines(data));
+}
---
+++ service.js
@@ -0,0 +1,11 @@
+//Function that will do something
+function doSomething(z) {
+  /*
+     let val = z / 2;
+     return val;
+  */
+}
+
+let x = 1;
+let y = 4;
+x = doSomething(y); // "X" would be undefined; may not be desirable
